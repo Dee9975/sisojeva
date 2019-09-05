11716e43d0217e74fe6159ec647f34ea0e5d80ad
Matiss Work <matisskalnins6@gmail.com>
2019-09-05 12:58:17 +0300

    Updatwe

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <string>
 
-class Hello {
-    public:
-        char name[20];
-        void sayHello() {
-            std::cout << "\nHello" << std::endl;
-        }
+using namespace std;
+
+class Audzeknis {
+  public:
+    //atributi
+    string vards;
+    string grupa;
+    unsigned kurss;
+
+    float getStipa(float videja, float kavejumi) {
+      if(kavejumi < 10 && videja > 4)
+        return videja * 10;
+      else 
+        return 10;
+    }
+
+    void printTings() {
+        std::cout << "\nAudzeknis " << vards;
+        std::cout << " no " << grupa << " kursa" << std::endl;
+    }
 };
 
-int main() {
-    Hello howdy;
-    howdy.sayHello();
+int main() 
+{
+  Audzeknis admins;
+  float videjaAtzime = 5.5;
+  int kavejumuSkaits = 3;
+  std::cout << "Ievadiet audzekna vardu un kursu: " << std::endl;
+  std::cin >> admins.vards;
+  std::cin >> admins.grupa;
+  admins.printTings();
+  std::cout << "sanem stipendiju: " << admins.getStipa(videjaAtzime, kavejumuSkaits) << " EURO" << std::endl;
 }
